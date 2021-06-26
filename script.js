fdbd7eace5164625ae697fd1d43d12488e120630
La'Tonia Mertica <latoniamertica@gmail.com>
2021-06-26 16:38:29 -0400

    fine tuned code/ stuck under bkgrd/overlay

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //REGEX CODE FOR NONITALIC PARENTHESES AROUND ITALIC PRONUNCIATION OF NAME IN FOOTER
-$("#str").html(function (i, h) {
-  return h.replace(/\(([^\)]+)\)/, "(<i>$1</i>)");
-});
+// $("#str").html(function (i, h) {
+//   return h.replace(/\(([^\)]+)\)/, "(<i>$1</i>)");
+// });
+
+const strElement = document.getElementById("str");
+// strElement.innerHTML = "change this";
+const replaced = strElement.innerHTML.replace(
+  /\(([^\)]+)\)/,
+  `(<span class="pronunciation">$1</span>)`
+);
+console.log(replaced);
+strElement.innerHTML = replaced;
