76648165503611c54a215d12f3a459be95c8a7cc
La'Tonia Mertica <latoniamertica@gmail.com>
2021-11-24 14:07:06 -0500

    first solid draft of feature section completed w/full script notes

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ const replaced = strElement.innerHTML.replace(
 );
 console.log(replaced);
 strElement.innerHTML = replaced;
+
+// //when we scroll
+// window.addEventListener("scroll", (evt) => {
+//   //get scroll height
+//   const scrollHeight = window.scrollY;
+
+//   //get overlay height
+//   const overlayHeight = document.getElementById("overlay").offsetHeight;
+//   const socials = document.getElementById("socials");
+//   console.log(overlayHeight);
+//   console.log(scrollHeight);
+
+//   //figure out if scroll height is more than overlay height.
+//   //MZM 11-17-21 TODO: Never got the math working on the psuedo code above
+//   if (scrollHeight > 115) {
+//     //hide the icons
+//     socials.style.display = "none";
+//   } else if (scrollHeight < 115) {
+//     //if you scroll back up, they should appear again
+//     socials.style.display = "block";
+//   }
+// });
